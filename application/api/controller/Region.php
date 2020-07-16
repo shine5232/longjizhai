12c14ad5707ea65f863b0282221868f9eb2b3a27
shine5232 <2692650429@qq.com>
2020-07-16 12:00:37 +0800

    整合接口文件

--- application/api/controller/Region.php
+++ application/api/controller/Region.php
@@ -9,12 +9,19 @@ use think\Validate;
 class Region extends Controller
 {
     protected $ret = ['code'=>0,'msg'=>"",'count'=>0,'data'=>[]];
+    /**
+     * 获取地区信息
+     */
     public function index(){
         $post = $this->request->post();
         if($post['type'] == '1'){
             $data = _getRegion();
         }else{
-            $data = _getRegion($post['code']);
+            if(isset($post['is_open'])){
+                $data = _getRegion($post['code'],true);
+            }else{
+                $data = _getRegion($post['code']);
+            }
         }
         $this->ret['data'] = $data;
         return json($this->ret);
