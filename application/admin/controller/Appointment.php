e0412d15592a34ce25ad27a8d9d611f117f28989
zhongs <16545797432qq.com>
2020-08-16 20:41:55 +0800

    预约管理

---
+++ application/admin/controller/Appointment.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace app\admin\controller;
+
+use \think\Db;
+
+class Appointment extends Main
+{
+    protected $ret = ['code' => 0, 'msg' => "", 'count' => 0, 'data' => []];
+    /**
+     * 认证管理-列表页面
+     */
+    public function index($type)
+    {
+        $this->assign('type', $type);
+        return $this->fetch('index');
+    }
+    /**
+     * 认证管理-列表页面接口数据
+     */
+    public function indexList()
+    {
+        $type  = $this->request->get('type');
+        $page = $this->request->param('page', 1, 'intval');
+        $limit = $this->request->param('limit', 20, 'intval');
+        $keywords = $this->request->param('keywords','');
+        $page_start = ($page - 1) * $limit;
+        $where = '';
+        if($keywords){
+            $where = " AND (A.name = '".$keywords."' OR B.uname = '".$keywords."')";
+        }
+        $sql = "SELECT CASE WHEN A.status = 0 THEN '未处理' WHEN A.status = 1 THEN '预约成功' ELSE '预约失败' END AS status_name,A.status ,A.note,A.name,A.id,B.uname AS appointmented_name,A.mobile,A.appoint_time,A.content
+                FROM lg_appointment A 
+                INNER JOIN lg_member B ON A.appointmented_uid = B.id 
+                WHERE B.type = " . $type . $where . "
+                ORDER BY A.id DESC
+                limit $page_start,$limit";
+        // var_dump($sql);die;
+        $data = Db::query($sql);
+        // var_dump($data);die;
+        $sql1 = "SELECT COUNT(1) AS count FROM lg_appointment A 
+        INNER JOIN lg_member B ON A.appointmented_uid = B.id 
+        WHERE B.type = " . $type . $where;
+        $count = Db::query($sql1);
+        // var_dump($count);die;
+        if ($data) {
+            $this->ret['count'] = $count[0]['count'];
+            $this->ret['data'] = $data;
+        }
+        return json($this->ret);
+    }
+
+    public function detail()
+    {
+        $id  = $this->request->get('id');
+        $this->assign('id', $id);
+        return $this->fetch('reply');
+    }
+
+    //回复处理
+    public function replyDetail()
+    {
+        $post =  $this->request->post();
+        $id = $post['id'];
+        // if($post['status'] == 2 && $post['note'] == null){
+        //     $this->ret['code'] = 0;
+        //     $this->ret['msg'] = '拒绝时备注必填';
+        // }
+        $res = Db::name('appointment')
+        ->where('id',$id)
+        ->update(['note'=>$post['note'],'status'=>$post['status']]);
+        // var_dump(Db::name('review_user')->getLastSql());
+        // die;
+        if ($res) {
+            $this->ret['code'] = 200;
+            $this->ret['msg'] = 'success';
+        }
+        return json($this->ret);
+    }
+
+    public function search(){
+        return $this->fetch('');
+    }
+}
