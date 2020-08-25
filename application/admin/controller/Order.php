1bfa3268a3896f0d510284eb6fc4ba84ade1c2c0
shine5232 <2692650429@qq.com>
2020-08-25 15:26:20 +0800

    订单管理

---
+++ application/admin/controller/Order.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace app\admin\controller;
+
+use \think\Db;
+use \think\Reuquest;
+
+class Order extends Main
+{
+    protected $ret = ['code' => 0, 'msg' => "", 'count' => 0, 'data' => []];
+    /**
+     * 订单管理-订单列表
+     */
+    public function index($status){
+        if(request()->isAjax()){
+            $page = $this->request->param('page', 1, 'intval');
+            $limit = $this->request->param('limit', 20, 'intval');
+            $status = $this->request->param('status','');
+            $order_sn = $this->request->param('order_sn','');
+            $uname = $this->request->param('uname','');
+            $page_start = ($page - 1) * $limit;
+            $where['A.status'] = ['eq',$status];
+            $and = ' AND 1';
+            if($order_sn){
+                $where['A.order_sn'] = ['eq',$order_sn];
+            }
+            if($uname){
+                $and = " AND B.uname = '$uname'";
+            }
+            $data = Db::name('order')->alias('A')
+                ->join('member B','B.uid = A.uid '.$and,'INNER')
+                ->join('shop C','C.id = A.shop_id','INNER')
+                ->where($where)
+                ->field('A.*,B.uname,C.name AS shop_name')
+                ->order('A.id DESC')->limit($page_start, $limit)->select();
+            $count = Db::name('order')->alias('A')
+                ->join('member B','B.uid = A.uid '.$and,'INNER')
+                ->join('shop C','C.id = A.shop_id','INNER')
+                ->where($where)->count();
+            if ($data) {
+                $this->ret['count'] = $count;
+                $this->ret['data'] = $data;
+            }
+            return json($this->ret);
+        }else{
+            $this->assign('status',$status);
+            return $this->fetch();
+        }
+    }
+    /**
+     * 订单管理-订单搜索
+     */
+    public function search(){
+        $status = $this->request->get('status');
+        $this->assign('status',$status);
+        return $this->fetch('search');
+    }
+    /**
+     * 订单管理-订单详情
+     */
+    public function detail(){
+        $id = $this->request->get('id');
+        $order = Db::name('order')->alias('A')
+            ->join('member B','B.uid = A.uid ','INNER')
+            ->join('shop C','C.id = A.shop_id','INNER')
+            ->join('member_address D','D.id = A.address_id','INNER')
+            ->field('A.*,B.uname,C.name AS shop_name,D.province,D.city,D.county,D.address,D.mobile,D.name AS get_name')
+            ->where('A.id',$id)->find();
+        if($order){
+            $order['info'] = Db::name('order_info')->alias('A')
+                ->join('shop_goods B','B.id = A.shop_goods_id','INNER')
+                ->join('shop_goods_attr C','C.id = A.goods_attrs_id','INNER')
+                ->where('A.order_id',$id)
+                ->field('A.goods_num,B.name,B.unit,C.name AS attr_name,C.price,C.shop_price,C.thumb,C.specs')
+                ->select();
+        }
+        $this->assign('order',$order);
+        return $this->fetch('detail');
+    }
+}
